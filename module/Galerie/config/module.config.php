0a0b1f60673c3375e980bd512c932561826fbe0e
Guillaume Debruyne <g.debruyne0@laposte.net>
2013-07-04 08:30:53 +0200

    N°commit:7a942ab Déclaration de la template_map (ch2.4.3, p101).

--- module/Galerie/config/module.config.php
+++ module/Galerie/config/module.config.php
@@ -6,6 +6,16 @@ return array(
 					),
 			),
 		'view_manager' => array(
+				'template_map' => array(
+					'galerie/index/index' =>
+				__DIR__ . '/../view/galerie/index/index.phtml',
+					'galerie/index/edit' =>
+				__DIR__ . '/../view/galerie/index/edit.phtml',
+					'galerie/index/del' =>
+				__DIR__ . '/../view/galerie/index/del.phtml',
+					'galerie/index/view' =>
+				__DIR__ . '/../view/galerie/index/view.phtml',
+				),
 				'template_path_stack' =>  array(
 						'galerie' => __DIR__ . '/../view',
 				),
