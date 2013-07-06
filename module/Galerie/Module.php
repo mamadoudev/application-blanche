368ec2403a752b165774552197f7bace3d6eef23
Guillaume Debruyne <g.debruyne0@laposte.net>
2013-07-06 15:40:19 +0200

    Préparation aux chargements des aides de vues, et aux services (module.php).

--- module/Galerie/Module.php
+++ module/Galerie/Module.php
@@ -4,11 +4,13 @@ namespace Galerie;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
 
 use Zend\EventManager\EventInterface;
 use Zend\Mvc\ModuleRouteListener;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface, BootstrapListenerInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface, BootstrapListenerInterface, ViewHelperProviderInterface
 {
 	public function getAutoloaderConfig()
 	{
@@ -32,5 +34,22 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface, Bo
 	public function onBootstrap(EventInterface $e)
 	{
 		$translator = $e->getApplication()->getServiceManager()->get('translator');
+	}
+	
+	public function getServiceConfig()
+	{
+		return array(
+			'factories' => array(
+			),
+		);
+	}
+	
+	public function getViewHelperConfig()
+	{
+		return array(
+			'factories' => array(
+				
+			),
+		);
 	}
 }
