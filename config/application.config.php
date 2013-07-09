b3888cf9b42b468c8268a43aae81c3b4e1385385
Guillaume Debruyne <g.debruyne0@laposte.net>
2013-07-09 14:21:25 +0200

    une petite coquille dans la config de chargement des modules.

--- config/application.config.php
+++ config/application.config.php
@@ -9,14 +9,7 @@ if ($env == 'development') {
 }
 return array(
     // This should be an array of module namespaces used in the application.
-    'modules' => array(
-        'Application',
-		'Galerie',
-    ),
     'modules' => $modules,
-
-
-
 
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
